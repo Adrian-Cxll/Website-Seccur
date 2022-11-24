a557cdcf432b016a9e6d7708a70ea706ff1012d1
Adrian <103188006+Adrian-Cxll@users.noreply.github.com>
2022-11-24 20:46:01 +0100

    dd

---
+++ main.js
@@ -0,0 +1,17 @@
+var Default = 1234;
+
+
+const LogInUtility = {
+    verify: async (pin) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (pin === Default.PIN) {
+                    resolve(true);
+                }
+                else {
+                    reject(`Invalid pin: ${pin}`);
+                }
+            }, N.rand(300, 700));
+        });
+    }
+};
